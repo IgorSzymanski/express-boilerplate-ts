4fc0e324b9f581f0e936a6fad10b7217d1572c29
Igor Szymański <igor.szymanski@me.com>
2020-06-08 08:14:16 +0200

    Cleaned code

--- src/domain-example/index.ts
+++ src/domain-example/index.ts
@@ -1,20 +1,22 @@
 import { makeWallet, Wallet, addPoints } from './wallet/Wallet'
-import { Result, fold, map, flatten, pipe, flow } from './Result'
+import { fold, map, flatten, pipe, flow } from './Result'
 import { makeSimplePoints } from './wallet/points/SimplePoints'
 
 const goodWallet = makeWallet('ThisIs@mail.com')
 const badWallet = makeWallet('thisisnotmail')
 
-const add5Points = pipe(makeSimplePoints(5), map(addPoints))
+const add5SimplePoints = pipe(makeSimplePoints(5), map(addPoints))
 const goodWalletWith5Points = pipe(
-  add5Points,
-  flow(map((addPoints) => pipe(goodWallet, map(addPoints)))),
-  flatten
+  add5SimplePoints,
+  flow(
+    map((addPoints) => pipe(goodWallet, map(addPoints))),
+    flatten
+  )
 )
 
 const onError = (error: Error) => console.log(`${error.name}: `, error.message, error.stack)
 const onWallet = (wallet: Wallet) => console.log(wallet)
-const printWallet = (maybeWallet: Result<Wallet>) => fold(onError, onWallet)(maybeWallet)
+const printWallet = fold(onError, onWallet)
 
 console.log(`\n~~~Domain Example module.~~~`)
 printWallet(goodWallet)
